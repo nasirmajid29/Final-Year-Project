be929f3307e66c9b6d06bba05e06db5dd634a121
Nasir <nasir.majid29@gmail.com/bin/python3>
2022-11-16 22:33:25 +0000

    added func to generate all point clouds for a eisode

--- depth_to_pc.py
+++ depth_to_pc.py
@@ -2,7 +2,7 @@ import open3d as o3d
 import numpy as np
 import pyvista
 
-def depth_to_pc(depth_path, colour_path):
+def rgb_depth_to_pc(colour_path, depth_path):
     depth = o3d.io.read_image(depth_path)
     colour = o3d.io.read_image(colour_path)
 
@@ -28,9 +28,22 @@ def visualise_pc(points, colours):
     plotter.add_points(points, opacity=1, point_size=1.5, render_points_as_spheres=True, scalars=colours, rgb=True)
     plotter.show()
 
+def generate_pc_for_episode(episode_size, episode_path, camera_type):
+
+    all_points = []
+    all_colours = []
+
+    for i in range(episode_size):
+        depth_path = episode_path+"/wrist_depth/" +i+".png"
+        rgb_path = episode_path+"/wrist_rgb/" +i+".png"
+        
+        points, colours = rgb_depth_to_pc(rgb_path, depth_path)
+        all_points.append(points)
+        all_colours.append(colours)
+    return all_points, all_colours
+
 wrist_depth = "/home/nasir/Desktop/Demos/reach_target/variation0/episodes/episode0/wrist_depth/0.png" 
 wrist_colour = "/home/nasir/Desktop/Demos/reach_target/variation0/episodes/episode0/wrist_rgb/0.png"
 
-points, colour = depth_to_pc(wrist_depth, wrist_colour)
-
-visualise_pc(p, c)
+points, colour = rgb_depth_to_pc(wrist_colour, wrist_depth)
+visualise_pc(points, colour)
