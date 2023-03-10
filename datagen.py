81d0425bd6bc1ca05721ec081b6e8b91b3992dfe
nm219 <nm219@ic.ac.uk>
2023-03-10 13:31:16 +0000

    added images and models

---
+++ datagen.py
@@ -0,0 +1,1 @@
+python3 /vol/bitbucket/nm219/RLBench/tools/dataset_generator.py --save_path=/vol/bitbucket/nm219/Demos --tasks=[task names]|task name --image_size=x,y --renderer=opengl --episodes_per_task=n --variations=1 --processes=1
