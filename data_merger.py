a73ceb16f8a7b1b61c9a578da66902654ddd8894
nm219 <nm219@ic.ac.uk>
2023-04-15 11:43:25 +0100

    added data merger and data gen

--- data_merger.py
+++ data_merger.py
@@ -5,19 +5,31 @@ import numpy as np
 
 # test = torch.load("/homes/nm219/Final-Year-Project/data/reach_red_ball/processed/data_test.pt")
 
-data1 = torch.load("/vol/bitbucket/nm219/data/reach_target_256size_1.pt")
-data2 = torch.load("/vol/bitbucket/nm219/data/reach_target_256size_2.pt")
-data3 = torch.load("/vol/bitbucket/nm219/data/reach_target_256size_3.pt")
-data4 = torch.load("/vol/bitbucket/nm219/data/reach_target_256size_4.pt")
+data1 = torch.load("/vol/bitbucket/nm219/data/reach_target_512size_1.pt")
+data2 = torch.load("/vol/bitbucket/nm219/data/reach_target_512size_2.pt")
+data3 = torch.load("/vol/bitbucket/nm219/data/reach_target_512size_3.pt")
+data4 = torch.load("/vol/bitbucket/nm219/data/reach_target_512size_4.pt")
+data5 = torch.load("/vol/bitbucket/nm219/data/reach_target_512size_5.pt")
+data6 = torch.load("/vol/bitbucket/nm219/data/reach_target_512size_6.pt")
+data7 = torch.load("/vol/bitbucket/nm219/data/reach_target_512size_7.pt")
+data8 = torch.load("/vol/bitbucket/nm219/data/reach_target_512size_8.pt")
+data9 = torch.load("/vol/bitbucket/nm219/data/reach_target_512size_9.pt")
+data10 = torch.load("/vol/bitbucket/nm219/data/reach_target_512size_10.pt")
 
 print(len(data1))
 print(len(data2))
 print(len(data3))
 print(len(data4))
+print(len(data5))
+print(len(data6))
+print(len(data7))
+print(len(data8))
+print(len(data9))
+print(len(data10))
 
-full_data = np.concatenate((data1, data2, data3, data4))
+full_data = np.concatenate((data1, data2, data3, data4, data5, data6, data7, data8, data9, data10))
 
 print(len(full_data))
 
-torch.save(full_data, "/vol/bitbucket/nm219/data/reach_target_256size.pt")
+torch.save(full_data, "/vol/bitbucket/nm219/data/reach_target_512size.pt")
       
--- datagen.py
+++ datagen.py
@@ -1,3 +1,8 @@
-python3 /vol/bitbucket/nm219/RLBench/tools/dataset_generator.py --save_path=/vol/bitbucket/nm219/Demos --tasks=[task names]|task name --image_size=x,y --renderer=opengl --episodes_per_task=n --variations=1 --processes=1
 
-xvfb-run -a -s "-screen 0 1400x900x24"
+xvfb-run -a -s "-screen 0 1400x900x24" python3 /vol/bitbucket/nm219/RLBench/tools/dataset_generator.py --save_path=/vol/bitbucket/nm219/Demos --tasks=[task names]|task name --image_size=x,y --renderer=opengl --episodes_per_task=n --variations=1 --processes=1
+
+
+close_box
+pick_and_lift
+plug_charger_in_power_supply
+take_off_weighing_scales
