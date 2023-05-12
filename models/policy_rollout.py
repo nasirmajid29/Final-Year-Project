fixed quaternion and framing issues

models/policy_rollout.py
@@ -149,10 +149,12 @@ for i in range(runs):
         gripper_pos = obs["gripper_pose"]
         gripper_coord, gripper_rotation_quat = gripper_pos[:3], gripper_pos[3:]
         gripper_rotation_matrix = quaternion_rotation_matrix(gripper_rotation_quat)
-        gripper_frame = create_transform(gripper_coord, gripper_rotation_matrix)
+        world_gripper_frame = create_transform(gripper_coord, gripper_rotation_matrix)
+
+        gripper_world_frame = np.linalg.inv(world_gripper_frame)
 
         full_pc_world_points, full_pc_world_colours = np.hsplit(full_colour_pc_world, 2)
-        full_pc_gripper = transform_point_cloud(gripper_frame, full_pc_world_points)
+        full_pc_gripper = transform_point_cloud(gripper_world_frame, full_pc_world_points)
         full_colour_pc_gripper = np.concatenate((full_pc_gripper, full_pc_world_colours), axis=1)
 
         full_colour_pc_gripper = torch.tensor(full_colour_pc_gripper, dtype=torch.float32)

utils/depth_to_pc.py
@@ -10,7 +10,7 @@ from rlbench.utils import get_stored_demos, ObservationConfig, _resize_if_needed
 from rlbench.backend.utils import image_to_float_array, rgb_handles_to_mask
 from pyrep.objects import VisionSensor
 
-from visualise import visualise_pc, visualise_pc_rgb, visualise_pc_rgb_many, visualise_actions, visualise_over_time, fake_visualise_pc_rgb
+from visualise import visualise_pc, visualise_pc_rgb, visualise_pc_rgb_many, visualise_actions, visualise_over_time, fake_visualise_pc_rgb, visualise_world
 
 from scipy.spatial.transform import Rotation as R
 
@@ -154,12 +154,12 @@ def depth_to_pc(obs_config, task_name):
     # lab path "/vol/bitbucket/nm219/Demos"
     # amount -1
     
-    demos = get_stored_demos(-1, False, "/vol/bitbucket/nm219/Demos", 0, task_name, obs_config, random_selection=False)#, from_episode_number=11)
+    demos = get_stored_demos(1, False, "/home/nasir/Desktop/Demos", 0, task_name, obs_config, random_selection=False, from_episode_number=9)
     return demos
 
 def transform_between_frames(frame1, frame2):
-    inverse = np.linalg.inv(frame1)
-    return np.matmul(inverse, frame2)
+    inverse = np.linalg.inv(frame2)
+    return np.matmul(frame1, inverse)
 
 def transform_point_cloud(transform, point_cloud)-> np.ndarray:
 
@@ -292,7 +292,7 @@ def create_transform(translation, rotation):
 
 # print(np.matmul(all_gripper_frames[0], all_actions[0]) - all_gripper_frames[1])
 
-demo_folder = "reach_target_10eps"
+demo_folder = "reach_target"
 print(demo_folder)
 config = get_config([128,128])
 demos = depth_to_pc(config, demo_folder)
@@ -396,6 +396,7 @@ for i in range(len(demos)):
     #     fake_visualise_pc_rgb(full_colour_pc_world)
 
     gripper_pcs = []
+    gripper_poses = []
 
     all_gripper_frames = []
     all_gripper_pc = []
@@ -407,18 +408,21 @@ for i in range(len(demos)):
         gripper_coord = gripper_pos[:3]
         gripper_rotation_quat = gripper_pos[3:]
         gripper_rotation_matrix = quaternion_rotation_matrix(gripper_rotation_quat)
-        gripper_frame = create_transform(gripper_coord, gripper_rotation_matrix)
+        world_gripper_frame = create_transform(gripper_coord, gripper_rotation_matrix)
+
+        gripper_world_frame = np.linalg.inv(world_gripper_frame)
 
         full_pc_world_points, full_pc_world_colours = np.hsplit(full_colour_pc_world, 2)
-        full_pc_gripper = transform_point_cloud(gripper_frame, full_pc_world_points)
+        full_pc_gripper = transform_point_cloud(gripper_world_frame, full_pc_world_points)
         full_colour_pc_gripper = np.concatenate((full_pc_gripper, full_pc_world_colours), axis=1)
 
-        # if i == 0:
-        #     gripper_pcs.append(full_colour_pc_gripper)
+        if i == 0:
+            gripper_poses.append(gripper_coord)
+            gripper_pcs.append(full_colour_pc_gripper)
         #     if j == len(demos[i]._observations)-1:
         #         visualise_pc_rgb_many(gripper_pcs)
         
-        all_gripper_frames.append(gripper_frame)
+        all_gripper_frames.append(gripper_world_frame)
         all_gripper_pc.append(full_colour_pc_gripper)
         all_gripper_states.append(gripper_open)
 
@@ -431,7 +435,7 @@ for i in range(len(demos)):
 
         next_frame = all_gripper_frames[index+1]
         next_gripper_state = all_gripper_states[index+1]
-        action = transform_between_frames(frame, next_frame)
+        action = transform_between_frames(next_frame, frame)
         
         # if not next_gripper_state:
         #     action[3,0] = 1
@@ -460,9 +464,13 @@ for i in range(len(demos)):
 
     # subsampled_actions.append(np.zeros((4,4)))
 
-    # if i == 0:
-    #     visualise_actions(all_actions)
-    #     visualise_over_time(gripper_pcs, all_actions)
+    if i == 0:
+        # visualise_actions(all_actions)
+        visualise_over_time(gripper_pcs, all_actions)
+        
+        gripper_pcs.append(full_colour_pc_world)
+
+        visualise_world(gripper_pcs, gripper_poses, all_actions)
 
 # print("Action size is:", np.array(all_actions).shape)
 # print("Number of gripper pointclouds is:", np.array(all_gripper_pc).shape)

utils/visualise.py
@@ -1,7 +1,7 @@
 
 import numpy as np
 import pyvista
-pyvista.start_xvfb()
+# pyvista.start_xvfb()
 
 
 def visualise_pc(point_cloud):
@@ -112,3 +112,32 @@ def visualise_policy(point_clouds, actions):
     plotter.add_mesh(polydata, color='yellow', point_size=20)
 
     plotter.screenshot("policy.png")
+
+def visualise_world(world, poses, actions):
+
+    plotter = pyvista.Plotter()
+    actions_list = []
+    
+    for point_cloud in world:
+    
+        points, colours = np.hsplit(point_cloud, 2)
+        plotter.add_points(points, opacity=1, point_size=4, render_points_as_spheres=True, scalars=colours.astype(int), rgb=True)
+
+    for pose in poses:
+    
+        plotter.add_points(pose, opacity=1, point_size=4, render_points_as_spheres=True)
+
+    for action in actions:
+    
+        action_translate = action[:3, 3].reshape(-1)
+        actions_list.append(action_translate)
+
+    centres = np.cumsum(actions_list, axis=0)
+    centres = [0,0,0] + centres[:-1]    
+
+
+    plotter.add_points(centres, opacity=1, point_size=4, render_points_as_spheres=True)
+    plotter.add_arrows(np.array(centres), np.array(actions_list[:-1]))
+        
+    plotter.add_axes_at_origin()
+    plotter.show()